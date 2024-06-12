75b2a2cf4b7e8e600d3fd13c6d20aef7ae9fde61
21Saliji <147812319+21Saliji@users.noreply.github.com>
2024-06-12 10:55:18 +0200

    Update script.js

--- Biltong-lovers-landing-page/script.js
+++ Biltong-lovers-landing-page/script.js
@@ -2,13 +2,14 @@ function buyBiltong() {
     let name = prompt("What is your name?");
     let email = prompt("What is your email address?");
 
-    if (name || email) {
+    if (name && email) {
         alert("Thank you, " + name + "! We'll be in touch by email");
     } else {
         alert("Please provide both your name and email address.");
     }
 }
 
-let buyButton = document.querySelector("button");
-
-buyButton.addEventListener("click", buyBiltong);
+document.addEventListener("DOMContentLoaded", function() {
+    let buyButton = document.getElementById("buyButton");
+    buyButton.addEventListener("click", buyBiltong);
+});
